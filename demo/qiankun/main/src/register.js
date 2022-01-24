c5687960a2e7d2276167d5c258f4264709e6cf94
taiwanwang <taiwangwan@163.com>
2022-01-24 18:10:52 +0800

    update

--- demo/qiankun/main/src/register.js
+++ demo/qiankun/main/src/register.js
@@ -3,19 +3,19 @@ import { registerMicroApps, start } from 'qiankun';
 registerMicroApps([
   {
     name: 'other-1', // app name registered
-    entry: '//10.18.28.35:8881',
+    entry: '//192.168.43.50:8881',
     container: '#other-1',
     activeRule: '/other-1',
   },
   {
     name: 'other-2', // app name registered
-    entry: '//10.18.28.35:3000',
+    entry: '//192.168.43.50:3000',
     container: '#other-1',
     activeRule: '/other-2',
   },
   {
     name: 'other-3', // app name registered
-    entry: '//10.18.28.35:8883',
+    entry: '//192.168.43.50:8883',
     container: '#other-1',
     activeRule: '/other-3',
   },
